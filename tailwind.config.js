be2628778e0c85df94b3cafe7544e6f953e6000a
Faris Dwi Ramadhan <farisdwiramadhan028@gmail.com>
2024-09-19 01:19:47 +0700

    Fixing Nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./resources/**/*.blade.php",
@@ -10,6 +11,9 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#227377',  // Warna kustom ditambahkan di sini
       },
     },
   },
